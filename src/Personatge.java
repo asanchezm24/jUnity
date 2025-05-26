e3f194f0ae385162062d9d1e400c79a4e3eeccfe
Xavi <garciax207@gmail.com>
2025-05-26 16:56:49 +0200

    Saltos

--- src/Personatge.java
+++ src/Personatge.java
@@ -6,7 +6,9 @@ public class Personatge extends PhysicBody {
 
 	private Input input;
 	boolean aterra = true;
-	int salts = 2;
+	boolean dobleSalto = true;
+	//TODO: Mejorar movimiento y saltos.
+
 
 	public Personatge(String name, int x1, int y1, int x2, int y2, double angle, String path, Field f) {
 		super(name, x1, y1, x2, y2, angle, path, f);
@@ -17,7 +19,7 @@ public class Personatge extends PhysicBody {
 	public void onCollisionEnter(Sprite sprite) {
 		if (sprite instanceof Roca && sprite.y1 >= this.y2) {
 			aterra = true;
-			salts = 2;
+			dobleSalto = true;
 		}
 	}
 
@@ -30,15 +32,20 @@ public class Personatge extends PhysicBody {
 
 	public void moviment(Input in) {
 
+
 		if (in == Input.DRETA) {
 			this.setVelocity(+5, this.velocity[1]);
 		}
 		if (in == Input.ESQUERRA) {
 			this.setVelocity(-5, this.velocity[1]);
 		}
-		if (in == Input.SALT && salts > 0) {
-			this.addForce(0, -2);
-			salts--;
+		if (in == Input.SALT) {
+			if(aterra) {
+				this.addForce(0, -1);
+			} else if (dobleSalto){
+				this.addForce(0,-1);
+				dobleSalto = false;
+			}
 		}
 		if(in.equals(Input.RES)){
 			this.setVelocity(0, this.velocity[1]);
