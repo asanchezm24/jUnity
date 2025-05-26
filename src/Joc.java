20732385006a1a9a2b2b208d81a05fe446e462c6
adrian <adrian.sanchez.joliver@gmail.com>
2025-05-26 17:02:13 +0200

    commit

--- src/Joc.java
+++ src/Joc.java
@@ -13,16 +13,31 @@ public class Joc {
 
 
 	public static void main(String[] args) throws InterruptedException {
-		Personatge pers = new Personatge("Adri",  250, 250, 300, 350, 0, "resources/Juego/mrPopuu.png", f);
-		
-        // Suelo - largo y fino
-        Roca terra = new Roca("terra", 0, 820, w.getWidth(), 830, 0, "resources/rock1.png", f);
+        
+        // Personaje más pequeño (ej. 50x75)
+        Personatge pers = new Personatge("Adri", 100, 745, 150, 820, 0, "resources/Juego/mrPopuu.png", f);
 
-        // Techo - largo y fino
-        Roca sostre = new Roca("sostre", 0, 0, w.getWidth(), 10, 0, "resources/rock1.png", f);
+        // Suelo
+        Roca terra = new Roca("terra", 0, 820, w.getWidth(), 850, 0, "resources/Juego/Suelo.png", f);
 
-        // Plataforma central
-        Roca plataforma = new Roca("plat", 300, 300, 700, 310, 0, "resources/rock1.png", f);
+        // Techo
+        Roca sostre = new Roca("sostre", 0, 0, w.getWidth(), 10, 0, "resources/Juego/Suelo.png", f);
+
+        // 12 plataformas pequeñas (128x32), distribuidas en distintos puntos
+        Roca[] plataformas = new Roca[12];
+
+        plataformas[0] = new Roca("plat1", 50, 700, 178, 732, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[1] = new Roca("plat2", 250, 650, 378, 682, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[2] = new Roca("plat3", 450, 600, 578, 632, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[3] = new Roca("plat4", 650, 550, 778, 582, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[4] = new Roca("plat5", 850, 500, 978, 532, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[5] = new Roca("plat6", 150, 450, 278, 482, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[6] = new Roca("plat7", 350, 400, 478, 432, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[7] = new Roca("plat8", 550, 350, 678, 382, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[8] = new Roca("plat9", 750, 300, 878, 332, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[9] = new Roca("plat10", 950, 250, 1078, 282, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[10] = new Roca("plat11", 200, 200, 328, 232, 0, "resources/Juego/plataforma192x64.png", f);
+        plataformas[11] = new Roca("plat12", 400, 150, 528, 182, 0, "resources/Juego/plataforma192x64.png", f);
 
 		crearCoins();
 		int i = 0;
