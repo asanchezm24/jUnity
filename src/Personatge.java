85ef6f4d38ba496139426c01a8a453d9a3551093
Xavi <garciax207@gmail.com>
2025-05-26 15:56:55 +0200

    Salto arreglado

--- src/Personatge.java
+++ src/Personatge.java
@@ -6,7 +6,7 @@ public class Personatge extends PhysicBody {
 
 	private Input input;
 	boolean aterra = true;
-	int salts = 0;
+	int salts = 2;
 
 	public Personatge(String name, int x1, int y1, int x2, int y2, double angle, String path, Field f) {
 		super(name, x1, y1, x2, y2, angle, path, f);
@@ -15,9 +15,9 @@ public class Personatge extends PhysicBody {
 
 	@Override
 	public void onCollisionEnter(Sprite sprite) {
-		if (sprite instanceof Roca) {
+		if (sprite instanceof Roca && sprite.y1 >= this.y2) {
 			aterra = true;
-			salts = 0;
+			salts = 2;
 		}
 	}
 
@@ -36,9 +36,9 @@ public class Personatge extends PhysicBody {
 		if (in == Input.ESQUERRA) {
 			this.setVelocity(-5, this.velocity[1]);
 		}
-		if (in == Input.SALT && salts < 2) {
-			this.addForce(0, -3);
-			this.salts++;
+		if (in == Input.SALT && salts > 0) {
+			this.addForce(0, -2);
+			salts--;
 		}
 		if(in.equals(Input.RES)){
 			this.setVelocity(0, this.velocity[1]);
